45c0c532889734cabc8c22067b09359885905596
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-13 21:14:59 +0600

    Create index.js

---
+++ HTTP/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handle root URL
+app.get("/", (req, res) => {
+  res.redirect("/register");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/register", (req, res) => {
+  const fullName = req.body.fullName;
+  const age = req.body.age;
+  res.send(`<h2>Your name is ${fullName} and age is ${age}</h2>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
